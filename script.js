51b15fe7de914b230d5d58e11bf63eb47df4dd1b
Sameer Baghel <sameerbaghel0001@gmail.com>
2026-05-19 11:02:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+let element = document.getElementById("level");
+
+let level = 0;
+let current = element;
+
+while (current.parentElement) {
+    level++;
+    current = current.parentElement;
+}
+
+alert(`The level of the element is: ${level}`);
